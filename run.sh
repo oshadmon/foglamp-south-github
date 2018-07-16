58cdca096ed0e86983dac0dcbe9418b5e8497b0d
Ori Shadmon <oshadmon@gmail.com>
2018-07-16 10:55:10 -0700

    Script that runs both GitHub_Data_Generator and System_Data:

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-if [ $# -ne 3 ];then
- echo "usage: user:password organization repo"
+if [ $# -ne 1 ];then
+ echo "usage: authentication file"
  exit 1
 fi
 
-user=$1
-org=$2 
-repo=$3
+auth_file=$1
 
 # Generate GitHub data and send it to JSON files
-python3 $HOME/foglamp-south-plugin/GitHub_Data_Generator/code/generate_json_files.py ${user} ${repo} -o ${org} 
+python3 $HOME/foglamp-south-plugin/GitHub_Data_Generator/code/generate_json_files.py ${auth_file}
+python3 $HOME/foglamp-south-plugin/System_Data/code/generate_system_data.py
 
 
