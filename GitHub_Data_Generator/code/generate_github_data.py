42447701ef8360de279842c9d3b2be01cd275c00
Ori Shadmon <oshadmon@gmail.com>
2018-07-26 15:08:19 -0700

    Generate data from GitHub and store it either into FogLAMP or JSON or both

--- GitHub_Data_Generator/code/generate_github_data.py
+++ GitHub_Data_Generator/code/generate_github_data.py
@@ -11,20 +11,29 @@ import asyncio
 import datetime
 import json
 import os
+import queue
 import requests
+import threading
 import uuid
 
 loop = asyncio.get_event_loop()
 
-def get_timestamp()->str:
+def get_timestamp(que:queue.Queue=None)->str:
+   """
+   Create a current timestamp
+   """
    timestamp=datetime.datetime.now() 
    timestamp=timestamp.strftime('%Y_%m_%d')
-   return timestamp
+   que.put(timestamp)
 
-def send_request(repo:str='', organization:str='', auth_pair:tuple=())->(requests.models.Response, requests.models.Response, requests.models.Response): 
+def send_request(traffic_que:queue.Queue=None, commits_que:queue.Queue=None, clones_que:queue.Queue()=None,  repo:str='', organization:str='', 
+                 auth_pair:tuple=())->(requests.models.Response, requests.models.Response, requests.models.Response): 
    """
    Send request to specific Github API endpoint
    :param:
+      traffic_que:queue.Queue - que to store traffic results 
+      commits_que:queue.Queue - que to store commits reults 
+      clones_que:queue.Queue - que to store clones results 
       repo:str - repository name
       organization:str - organization name 
       auth_pair:tuple - user/password tuple pair
@@ -50,12 +59,12 @@ def send_request(repo:str='', organization:str='', auth_pair:tuple=())->(request
    base_url = base_url + organization + '/' + repo + '/traffic/clones'
    clones_response = requests.get(base_url, auth=auth_pair)
 
-
-   return traffic_response, commits_response, clones_response
+   traffic_que.put(traffic_response) 
+   commits_que.put(commits_response)
+   clones_que.put(clones_response)
   
 
-def read_traffic(traffic:requests.models.Response=None, repo:str='repo_name', json_dir:str='/tmp', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'),
-                 host:str='localhost', port:int=6683): 
+def read_traffic(traffic:requests.models.Response=None, repo:str='repo_name', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'))->list:
    """
    Create list of JSON objects with traffic info, and send to FogLAMP  
    :param: 
@@ -75,8 +84,6 @@ def read_traffic(traffic:requests.models.Response=None, repo:str='repo_name', js
    """
    traffic=traffic.json()
    data=[]
-   file_name=json_dir+'/'+'%s_github_%s_traffic_data.json' % (repo, timestamp)
-   open(file_name, 'w').close()
    for key in traffic['views']: 
       timestamp=datetime.datetime.strptime(key['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
       data.append(json.dumps({'timestamp' : str(timestamp), 
@@ -84,10 +91,9 @@ def read_traffic(traffic:requests.models.Response=None, repo:str='repo_name', js
                               'asset'     : 'github/%s/traffic' % repo, 
                               'readings'  : {'traffic' : key['uniques']}
                             }))
-   loop.run_until_complete(send_to_foglamp(data, host, port))
+   return data
 
-def read_commits_timestamp(commits:requests.models.Response=None, repo:str='repo_name', json_dir:str='/tmp', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'),
-                           host:str='localhost', port:int=6683): 
+def read_commits_timestamp(commits:requests.models.Response=None, repo:str='repo_name', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'))->list:
    """
    Create list of JSON objects with commits (by timestamp) info, and send to FogLAMP 
    :param: 
@@ -103,7 +109,6 @@ def read_commits_timestamp(commits:requests.models.Response=None, repo:str='repo
         "readings"  : {"commits/timestamp" : 12}
    }
    """
-   file_name=json_dir+'/'+'%s_github_%s_commits_timestamp.json' % (repo, timestamp) 
    commits=commits.json()
 
    # Group by TIMESTAMP
@@ -116,7 +121,6 @@ def read_commits_timestamp(commits:requests.models.Response=None, repo:str='repo
       timestamps[timestamp]+=1 
 
    # Create file with JSON object based on timestamps dict 
-   open(file_name, 'w').close()
    data=[]
    for key in timestamps: 
       data.append(json.dumps({'timestamp' : str(key),
@@ -124,10 +128,9 @@ def read_commits_timestamp(commits:requests.models.Response=None, repo:str='repo
                               'asset'     : 'github/%s/commits/timestamp' % repo, 
                               'readings'  : {'commits/timestamp' : timestamps[key]}
                             }))
-   loop.run_until_complete(send_to_foglamp(data, host, port))
+   return data 
 
-def read_commits_users(commits:requests.models.Response=None, repo:str='repo_name',  json_dir:str='/tmp', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'),
-                       host:str='localhost', port:int=6683): 
+def read_commits_users(commits:requests.models.Response=None, repo:str='repo_name', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'))->list:
    """
    Create list of JSON objects with commits (by user) info, and send to FogLAMPe
    :param: 
@@ -152,10 +155,6 @@ def read_commits_users(commits:requests.models.Response=None, repo:str='repo_nam
          users[user]=0
       users[user]+=1
 
-   # Create file with JSON objects based on users dict
-   file_name=json_dir+'/'+'%s_github_%s_commits_users.json' % (repo, timestamp)
-   open(file_name, 'w').close()
-   # number of users commit  
    data.append(json.dumps({'timestamp' : str(datetime.datetime.now()),    
                            'key'       : str(uuid.uuid1()),
                            'asset'     : 'github/%s/commits/users' % repo, 
@@ -168,10 +167,9 @@ def read_commits_users(commits:requests.models.Response=None, repo:str='repo_nam
                               'asset'     : 'github/%s/commits/users/%s' % (repo, key.replace(' ','-').replace('_', '-')),
                               'readings' : {'commits/users/%s' % key.replace(' ','-').replace('_', '-') : users[key]}
                             })) 
-   loop.run_until_complete(send_to_foglamp(data, host, port))
+   return data
 
-def read_clones(clones:requests.models.Response=None, repo:str='repo_name', json_dir:str='/tmp', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'),
-                host:str='localhost', port:int=6683):
+def read_clones(clones:requests.models.Response=None, repo:str='repo_name', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'))->list: 
    """
    Create list of JSON objects with daily clones, and write to file 
    :param: 
@@ -189,8 +187,6 @@ def read_clones(clones:requests.models.Response=None, repo:str='repo_name', json
    """
    clones=clones.json()
    data=[]
-   file_name=json_dir+'/'+'%s_github_%s_clones.json' % (repo, timestamp)
-   open(file_name, 'w').close()
    for key in clones['clones']:
       timestamp=datetime.datetime.strptime(key['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
       data.append(json.dumps({'timestamp' : str(key['timestamp']),
@@ -198,7 +194,7 @@ def read_clones(clones:requests.models.Response=None, repo:str='repo_name', json
                               'asset'     : 'github/%s/clones' % repo,
                               'readings'  : {'clones' : key['uniques']}
                             }))
-   loop.run_until_complete(send_to_foglamp(data, host, port))
+   return data 
 
 async def send_to_foglamp(data:list=[], arg_host:str='localhost', arg_port:int=6683): 
    """
@@ -222,18 +218,40 @@ async def send_to_foglamp(data:list=[], arg_host:str='localhost', arg_port:int=6
                print("Server error | code:{}, reason: {}".format(status_code, resp.reason))
                exit(1)
 
+def write_to_file(file_name:str='/tmp/data.json', data:list=[]):
+   """
+   Write data into JSON file 
+   :args: 
+      file_name:str - File to store JSON data into 
+      data:list - list of JSON objects
+   """
+   with open(file_name, 'a') as f:
+      for obj in data:
+         f.write(obj)
+         f.write('\n')
+
 def main():
     """
     Main
     :positional arguments:
-        auth_file   authentication file
+       auth_file             authentication file
+       host                  FogLAMP POST Host
+       port                  FogLAMP POST Port
+
+    :optional arguments:
+       -h, --help            show this help message and exit
+       -s SEND, --send SEND  Where to send the data to (foglamp|json|both)
+       -d DIR, --dir DIR     directory to send data into (for JSON)
+
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('auth_file', default='$HOME/foglamp-south-plugin/GitHub_Data_Generator/other/auth_pair.txt', help='authentication file')
     parser.add_argument('host', default='localhost', help='FogLAMP POST Host')
     parser.add_argument('port', default=6683, help='FogLAMP POST Port')
-
+    parser.add_argument('-s', '--send', default='foglamp', help='Where to send the data to (foglamp|json|both)')
+    parser.add_argument('-d', '--dir', default='/tmp', help='directory to send data into (for JSON)')
     args = parser.parse_args()
+
     env=os.path.expanduser(os.path.expandvars(args.auth_file))
     with open(env, 'r') as f:
        output=f.read().replace('\n','').split(' ')
@@ -242,14 +260,53 @@ def main():
     org=output[2]
     json_dir=output[3]
 
-    timestamp=get_timestamp()
+    # Generate data 
+    timestamp_que=queue.Queue()
+    traffic_que=queue.Queue()
+    commits_que=queue.Queue()
+    clones_que=queue.Queue()
+    threads=[threading.Thread(target=get_timestamp, args=(timestamp_que,)), 
+             threading.Thread(target=send_request,  args=(traffic_que, commits_que, clones_que, repo, org, auth,))
+            ]    
+    for thread in threads:
+       thread.start()
+    for thread in threads:
+       thread.join()
+    timestamp=timestamp_que.get()
+    traffic_request=traffic_que.get() 
+    commits_request=commits_que.get() 
+    clones_request=clones_que.get() 
 
-    traffic_response, commits_response, clones_response=send_request(repo, org, auth)
+    traffic_data=read_traffic(traffic_request, repo, timestamp) 
+    commits_timestamp_data=read_commits_timestamp(commits_request, repo, timestamp)
+    commits_users_data=read_commits_users(commits_request, repo, timestamp) 
+    clones_data=read_clones(clones_request, repo, timestamp) 
+    
+    file_name=args.dir+'/%s_github_%s_data.json' % (datetime.datetime.now().strftime('%Y_%m_%d'), repo) 
+    loop = asyncio.get_event_loop()
+    if args.send.lower() == 'foglamp': # Send to FogLAMP 
+       loop.run_until_complete(send_to_foglamp(traffic_data, args.host, args.port)) 
+       loop.run_until_complete(send_to_foglamp(commits_timestamp_data, args.host, args.port))
+       loop.run_until_complete(send_to_foglamp(commits_users_data, args.host, args.port))
+       loop.run_until_complete(send_to_foglamp(clones_data, args.host, args.port))
+    elif args.send.lower() == 'json': # Send to JSON file
+       open(file_name, 'w').close()
+       write_to_file(file_name, traffic_data)
+       write_to_file(file_name, commits_timestamp_data)
+       write_to_file(file_name, commits_users_data) 
+       write_to_file(file_name, clones_data)
+    else: # If something else write to both
+       loop.run_until_complete(send_to_foglamp(traffic_data, args.host, args.port))
+       loop.run_until_complete(send_to_foglamp(commits_timestamp_data, args.host, args.port))
+       loop.run_until_complete(send_to_foglamp(commits_users_data, args.host, args.port))
+       loop.run_until_complete(send_to_foglamp(clones_data, args.host, args.port))
+       open(file_name, 'w').close()
+       write_to_file(file_name, traffic_data)
+       write_to_file(file_name, commits_timestamp_data)
+       write_to_file(file_name, commits_users_data)
+       write_to_file(file_name, clones_data)
 
-    read_traffic(traffic_response, repo, json_dir, timestamp, args.host, args.port)
-    read_clones(clones_response, repo, json_dir, timestamp, args.host, args.port)
-    read_commits_timestamp(commits_response, repo, json_dir, timestamp, args.host, args.port) 
-    read_commits_users(commits_response, repo, json_dir, timestamp, args.host, args.port)
+  
 
 if __name__ == '__main__': 
    main()
