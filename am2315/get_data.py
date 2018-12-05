f45a866afe49415e065d3d4d544033c004490f26
Ori Shadmon <oshadmon@gmail.com>
2018-12-05 15:40:37 -0800

    am2315 sensor

---
+++ am2315/get_data.py
@@ -0,0 +1,137 @@
+# Based on - https://github.com/adafruit/Adafruit_CircuitPython_AM2320/blob/master/adafruit_am2320.py
+
+from pyftdi.i2c import I2cController, I2cNackError, I2cPort
+import time
+try:
+    import struct
+except ImportError:
+    import ustruct as struct
+
+
+#pylint: disable=bad-whitespace
+# Internal constants:
+AM2320_DEFAULT_ADDR = 0x5C
+AM2320_CMD_READREG  = 0x03
+AM2320_REG_TEMP_H   = 0x02
+AM2320_REG_HUM_H    = 0x00
+LENGTH              = 2
+
+def _crc16(data):
+    crc = 0xffff
+    for byte in data:
+        crc ^= byte
+        for _ in range(8):
+            if crc & 0x0001:
+                crc >>= 1
+                crc ^= 0xA001
+            else:
+                crc >>= 1
+    return crc
+
+
+class GetData:
+   def __init__(self): 
+      i2c=self.__init_i2c_controller()
+      self.slave=self.__i2c_slave_port(i2c) 
+
+   def __init_i2c_controller(self)->I2cController:
+      """
+      Initiate the I2C controller
+      :param:
+          i2c:pyftdi.i2c.I2cController - I2C controller object 
+      :return: 
+          I2C object 
+      """
+      i2c=I2cController()
+      i2c.set_retry_count(1)
+      i2c.configure('ftdi://ftdi:232h:FT2BZGR5/1')
+      return i2c
+
+   def __i2c_slave_port(self, i2c:I2cController=None)->I2cPort:
+      """
+      Get slave port 
+      :args: 
+         i2c:pyftdi.i2c.I2cController - I2C controller object 
+      :param: 
+         slave:pyftdi.i2c.I2cPort - 
+      :return: 
+         slave port object 
+      """
+      try: 
+         slave = i2c.get_port(AM2320_DEFAULT_ADDR)
+      except: 
+         print('Unable to get Port for %s' % AM2320_DEFAULT_ADDR)
+         exit(1)
+
+      for i in range(10): 
+          try:
+              slave.read(0)
+              return slave
+          except:
+              pass
+          time.sleep(0.25)
+      raise ValueError("AM2320 not found")
+
+   def _wake_sensor(self): 
+       """
+       Wake sensor
+       """
+       try:
+           self.slave.write(bytes([0x00]))
+       except OSError: 
+           pass
+
+   def _read_register(self, reg:int=0x00, length:int=0)->int:
+       """
+       Read from register 
+       :args: 
+          reg:int - register to read from 
+          length:int - legister length 
+       :param:
+       :return:
+          result from read
+       """
+       self._wake_sensor()
+       cmd = [reg & 0xFF, length]
+       self.slave.write_to(AM2320_CMD_READREG, bytearray(cmd))
+       result = self.slave.read(readlen=length+4)
+
+       if result[0] != 0x3 or result[1] != length:
+          raise RuntimeError('I2C read failure')
+       # Check CRC on all but last 2 bytes
+       crc1 = struct.unpack("<H", bytes(result[-2:]))[0]
+       crc2 = _crc16(result[0:-2])
+       if crc1 != crc2:
+          raise RuntimeError('CRC failure 0x%04X vs 0x%04X' % (crc1, crc2))
+
+       return result[2:-2]
+
+   def temperature(self)->float:
+       """
+       Get temperature
+       :return: 
+          temperature
+       """
+       temp=self._read_register(AM2320_REG_TEMP_H, 2)
+       temperature=struct.unpack(">H", temp)[0] 
+       if temperature >= 32768:
+          temperature = 32768 - temperature
+       return temperature/10.0
+
+   def humidity(self)->float: 
+       """
+       Get humidity
+       :return: 
+          humidity
+       """
+       humid=self._read_register(AM2320_REG_HUM_H, 2)
+       humidity=struct.unpack(">H", humid)[0]
+       return humidity/10.0
+
+
+if __name__ == '__main__': 
+    gd=GetData()
+    print(gd.temperature())
+    time.sleep(1)
+    print(gd.humidity())
+
