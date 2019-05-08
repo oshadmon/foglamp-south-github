3881a9d45d54a252c88db8569429877a647c8fe9
Ori Shadmon <oshadmon@gmail.com>
2019-05-08 11:45:53 -0700

    Current sample code

--- phidgets/current.py
+++ phidgets/current.py
@@ -5,23 +5,21 @@ URL: https://www.phidgets.com/?tier=3&catid=10&pcid=8&prodid=975
 """
 
 import time
-from Phidget22.Devices.Spatial import *
-from Phidget22.Devices.Accelerometer import * 
-from Phidget22.Devices.Gyroscope import * 
-from Phidget22.Devices.Magnetometer import * 
+from Phidget22.Devices.CurrentInput import *
 from Phidget22.PhidgetException import *
 from Phidget22.Phidget import *
 from Phidget22.Net import *
 
-ch = Accelerometer()
+ch = CurrentInput()
 ch.setDeviceSerialNumber(561266) 
-ch.setHubPort(4) 
+ch.setHubPort(3) 
 ch.setIsHubPortDevice(False) 
 ch.setChannel(0) 
 ch.openWaitForAttachment(5000)
 try: 
-   ch.getAcceleration() 
+   ch.getCurrent() 
 except Exception as e: 
     pass 
+
 time.sleep(5) 
-print(ch.getAcceleration())
+print(ch.getCurrent())
