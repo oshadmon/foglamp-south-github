439f04d35b5cb537138ca9eaa5bb120dedaf3c75
Ori Shadmon <oshadmon@gmail.com>
2019-05-08 01:27:36 +0000

    spatial sensor code

--- phidgets/spatial_motion.py
+++ phidgets/spatial_motion.py
@@ -1,3 +1,9 @@
+"""
+Name: Ori Shadmon 
+description: code for Spatial sensors (Acceleration, Gyroscope, Magnometer)
+URL: https://www.phidgets.com/?tier=3&catid=10&pcid=8&prodid=975
+"""
+
 import time
 from Phidget22.Devices.Spatial import *
 from Phidget22.Devices.Accelerometer import * 
@@ -21,6 +27,7 @@ spatial.saveMagnetometerCorrectionParameters()
 # Re-zeros the gyroscope in 1-2 seconds. 
 spatial.zeroGyro()
 
+# Acceleration 
 accelerometer = Accelerometer()
 accelerometer.setDeviceSerialNumber(561266)
 accelerometer.setHubPort(4)
@@ -29,9 +36,41 @@ accelerometer.setChannel(0)
 accelerometer.openWaitForAttachment(5000)
 
 try: 
-   accelerometer.
-https://www.phidgets.com/?tier=3&catid=10&pcid=8&prodid=975
+   accelerometer.getAcceleration()
+except Exception as e: 
+   pass 
+time.sleep(2) 
+print(accelerometer.getAcceleration()) 
 
+# Gyroscope
+gyroscope = Gyroscope() 
+gyroscope.setDeviceSerialNumber(561266)
+gyroscope.setHubPort(4)
+gyroscope.setIsHubPortDevice(False)
+gyroscope.setChannel(0)
 
+gyroscope.openWaitForAttachment(5000)
+
+try: 
+   gyroscope.getAngularRate()
+except Exception as e: 
+   pass 
+time.sleep(2) 
+print(gyroscope.getAngularRate()) 
+
+# Magnetometer 
+magnetometer = Magnetometer() 
+magnetometer.setDeviceSerialNumber(561266)
+magnetometer.setHubPort(4)
+magnetometer.setIsHubPortDevice(False)
+magnetometer.setChannel(0)
+magnetometer.openWaitForAttachment(5000)
+
+try: 
+    magnetometer.getMagneticField()
+except Exception as e: 
+    pass 
+time.sleep(2) 
+print(magnetometer.getMagneticField())
 
 
