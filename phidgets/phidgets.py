59a8defa8e5bf79ab7a3d693e17e70c58731495e
Ori Shadmon <oshadmon@gmail.com>
2019-05-09 10:58:02 -0700

    pretesting

--- phidgets/phidgets.py
+++ phidgets/phidgets.py
@@ -1,11 +1,18 @@
 import copy
 import uuid
 import logging
-
 import math
+
 from foglamp.common import logger
 from foglamp.plugins.common import utils
 from foglamp.services.south import exceptions
+
+from Phidget22.Devices.CurrentInput import *
+from Phidget22.Devices.HumiditySensor import *
+from Phidget22.Devices.TemperatureSensor import *
+from Phidget22.PhidgetException import *
+from Phidget22.Phidget import *
+
 
 __author__ = "Ori Shadmon"
 __copyright__ = "Copyright (c) 2019 Dianomic Systems"
@@ -32,7 +39,46 @@ def plugin_init(config):
          - .setIsHubPortDevice
          - .setChannel
       """ 
+      # humidity 
+      data['humidity'] = HumiditySensor()
+      data['humidity'].setDeviceSerialNumber(561266)
+      data['humidity'].setHubPort(5)
+      data['humidity'].setIsHubPortDevice(False)
+      data['humidity'].setChannel(0)
+
+      # temperature 
+      data['temp'] = TemperatureSensor()
+      data['temp'].setDeviceSerialNumber(561266)
+      data['temp'].setHubPort(5)
+      data['temp'].setIsHubPortDevice(False)
+      data'[temp'].setChannel(0)
+
+      # Current 
+      data['current'] = CurrentInput()
+      data['current'].setDeviceSerialNumber(561266)
+      data['current'].setHubPort(3)
+      data['current'].setIsHubPortDevice(False)
+      data['current'].setChannel(0)
+
       # for each sensor do try/catch (pass) on the first get
+      data['humidity'].openWaitForAttachment(5000)
+      try: 
+         data['humidity'].getHumidity()
+      except Exception as e:
+         pass 
+
+      data['temp'].openWaitForAttachment(5000)
+      try:
+         data['temp'].getTemperature()
+      except Exception as e:
+         pass
+
+      data['current'].openWaitForAttachment(5000)
+      try: 
+         ch.getCurrent()
+      except Exception as e: 
+         pass 
+
    except Exception as e: 
       _LOGGER.exception("SLTC exception: {}".format(str(ex)))
       raise ex 
@@ -57,7 +103,25 @@ def plugin_pull(handle):
          - key 
          -readings {} 
       """ 
-      data.append()
+      data.append({
+         'asset': 'temperature_and_humidity', 
+         'timestamp': timestamp,
+         'key': str(uuid.uuid4()), 
+         'readings': { 
+            "humidity": data['humidity'].getHumidity(), 
+            "temperature": data['temp'].getTemperature()
+         }
+      })
+
+      data.append({
+         'asset': 'current_input', 
+         'timestamp': timestamp, 
+         'key': str(uuid.uuid4())
+         'readings': { 
+            "current_input": data['current'].getCurrent() 
+         }
+      }) 
+
    except (Exception, RuntimeError) as ex:
       _LOGGER.exception("SLTC exception: {}".format(str(ex)))
       raise exceptions.DataRetrievalError(ex)
