7dfdf3d86e402c67e49e7b40e7b5b0f03b0e7887
Ori Shadmon <oshadmon@gmail.com>
2019-05-10 15:27:40 -0700

    sample code for encoder

---
+++ phidgets/encoder.py
@@ -0,0 +1,28 @@
+import time
+from Phidget22.Devices.Encoder import *
+from Phidget22.PhidgetException import *
+from Phidget22.Phidget import *
+
+# code for ch sensor
+ch = Encoder()
+ch.setDeviceSerialNumber(538854)
+ch.setHubPort(1)
+ch.setIsHubPortDevice(False)
+ch.setChannel(0)
+ch.openWaitForAttachment(5000)
+
+try:
+   ch.getPosition()
+except Exception as e:
+   pass
+time.sleep(.2)
+previous=0
+current=0
+while True:
+  current=ch.getPosition()
+  rpm = (current - previous)/1200
+  print(rpm)
+  previous = current
+
+  time.sleep(1)
+
